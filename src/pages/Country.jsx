c5645a644eef35f5ecd5de4fbfd0a72be8fbe927
Sebaplz <sebastian.h.neira@outlook.com>
2023-08-03 01:11:13 -0400

    Se agregan Border Countries y mobile listo

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -33,10 +33,6 @@ function Country() {
     return idiomasDisponibles?.common || datosPais.name.common;
   }
 
-  function obtenerNombresIdiomas(languages) {
-    return Object.values(languages);
-  }
-
   function obtenerMonedas(currencies) {
     return Object.values(currencies);
   }
@@ -55,10 +51,10 @@ function Country() {
     );
   } else {
     return (
-      <div className="flex flex-col gap-4 mx-8">
+      <div className="flex flex-col gap-4 p-8">
         <Link
           to="/"
-          className="text-white bg-[#2B3945] flex items-center justify-center gap-2 px-4 py-2 w-28 rounded-lg mt-10"
+          className="text-white bg-[#2B3945] flex items-center justify-center gap-2 px-4 py-2 w-28 rounded-lg"
         >
           <FaArrowLeft />
           Back
@@ -67,37 +63,83 @@ function Country() {
         {country.map((c) => (
           <div
             key={c.name.common}
-            className="flex flex-col lg:flex-row items-center"
+            className="flex flex-col lg:flex-row lg: items-center gap-4"
           >
-            <img src={c.flags.png} alt="" className="w-[320px] h-[220px]" />
+            <img
+              src={c.flags.png}
+              alt={c.flags.alt}
+              className="w-[320px] h-[220px]"
+            />
             <div className="flex flex-col gap-4 text-white w-full">
               <h2 className="text-xl font-bold mt-4">{c.name.common}</h2>
-              <h3>
-                Native Name: <span>{obtenerNombreComun(c)}</span>
+              <h3 className="font-semibold">
+                Native Name:{" "}
+                <span className="font-medium text-gray-300">
+                  {obtenerNombreComun(c)}
+                </span>
               </h3>
-              <h3>
-                Population: <span>{c.population.toLocaleString()}</span>
+              <h3 className="font-semibold">
+                Population:{" "}
+                <span className="font-medium text-gray-300">
+                  {c.population.toLocaleString()}
+                </span>
               </h3>
-              <h3>
-                Region: <span>{c.region}</span>
+              <h3 className="font-semibold">
+                Region:{" "}
+                <span className="font-medium text-gray-300">{c.region}</span>
               </h3>
-              <h3>
-                Sub Region: <span>{c.subregion}</span>
+              <h3 className="font-semibold">
+                Sub Region:{" "}
+                <span className="font-medium text-gray-300">{c.subregion}</span>
               </h3>
-              <h3>
-                Capital: <span>{c.capital[0]}</span>
+              <h3 className="font-semibold">
+                Capital:{" "}
+                <span className="font-medium text-gray-300">
+                  {c.capital[0]}
+                </span>
               </h3>
             </div>
-            <div className="mt-10 flex flex-col gap-4 text-white">
-              <h3>
-                Top Level Domain: <span>{c.tld[0]}</span>
+            <div className="flex flex-col gap-4 text-white w-full mt-4">
+              <h3 className="font-semibold">
+                Top Level Domain:{" "}
+                <span className="font-medium text-gray-300">{c.tld[0]}</span>
               </h3>
-              <h3>
-                Currencies: <span>{obtenerMonedas(c.currencies)[0].name}</span>
+              <h3 className="font-semibold">
+                Currencies:{" "}
+                <span className="font-medium text-gray-300">
+                  {obtenerMonedas(c.currencies)[0].name}
+                </span>
               </h3>
-              <h3>
-                Languages: <span>{obtenerNombresIdiomas(c.languages)}</span>
+              <h3 className="font-semibold">
+                Languages:{" "}
+                {Object.keys(c.languages).map((language, index) => (
+                  <span key={language} className="font-medium text-gray-300">
+                    {c.languages[language]}
+                    {index !== Object.keys(c.languages).length - 1 ? ", " : ""}
+                  </span>
+                ))}
               </h3>
+            </div>
+            <div className="w-full">
+              <h2 className="text-white font-semibold text-xl">
+                Border Countries:
+              </h2>
+              <div className="flex gap-4">
+                {c.borders && c.borders.length > 0 ? (
+                  c.borders.map((border) => (
+                    <h3
+                      key={border}
+                      className="text-white mt-4 py-1 px-4 bg-[#2B3945] rounded"
+                    >
+                      {border}
+                    </h3>
+                  ))
+                ) : (
+                  <h3 className="text-white mt-4 py-1 px-4 bg-[#2B3945] rounded">
+                    No específica
+                  </h3>
+                )}
+              </div>
             </div>
           </div>
         ))}
