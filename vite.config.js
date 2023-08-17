74d2e23734f43b55ca80f59bf5b2d5f9f614718a
Sebaplz <sebastian.h.neira@outlook.com>
2023-08-17 19:28:09 -0400

    Ready to deploy... 🧙‍♂️

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://sebaplz.github.io/Rest-Countries/",
+  base: "/Rest-Countries/",
 });
